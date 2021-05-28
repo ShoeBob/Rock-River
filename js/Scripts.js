3be4eaae0a7ea22746be5828ee0edf1e4c317698
Steven Schumacher <shoedds@hotmail.com>
2021-05-27 18:55:21 -0600

    Modal

--- js/Scripts.js
+++ js/Scripts.js
@@ -11,5 +11,7 @@ $(document).ready(function() {
           $(".navbar").removeClass("bg-light");
         }
     });
- 
+    $("#scheduleButton").click(function(){
+        $("#scheduleModal").modal("show");
+    });
 });
